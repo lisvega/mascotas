5c8a6f1d1d086c13b82a02de732fdbce5c3c1467
Ramon Williams <ramnwor21@gmail.com>
2022-08-25 20:15:05 +0200

    Creación de archivo delete-file, archivo  file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const {connect} = require("./helpers/db")
+const {setUpCloudinary} = require("./helpers/cloudinary")
 
 dotenv.config();
 const PORT = process.env.PORT;
-connect()
+connect(); 
+setUpCloudinary();
 
 const app = express();
 app.listen(PORT, () => {
