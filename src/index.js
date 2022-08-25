d26bdd36f99b2a95dd5ae559bba426b1bfac21d4
Ramon Williams <ramnwor21@gmail.com>
2022-08-25 19:29:23 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const {connect} = require("./helpers/db")
 
 dotenv.config();
 const PORT = process.env.PORT;
+connect()
 
 const app = express();
 app.listen(PORT, () => {
