2b54416730178e06d2c25a7201401389291861db
lisvega <lissyrivera456@gmail.com>
2022-08-25 21:04:54 +0200

    creación del index a falta de las rutas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,58 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const {connect} = require("./helpers/db")
-const {setUpCloudinary} = require("./helpers/cloudinary")
+const cors = require("cors");
+const { connect } = require("./helpers/db");
+const { setUpCloudinary } = require("./helpers/cloudinary");
+
+const AcousticRoutes = require("./api/acoustic/acoustic.routes");
+const ElectricRoutes = require("./api/electric/electric.routes");
+const UserRoutes = require("./api/user/user.routes");
 
 dotenv.config();
 const PORT = process.env.PORT;
-connect(); 
-setUpCloudinary();
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
+connect();
+
+setUpCloudinary();
+
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+   res.header("Access-Control-Allow-Credentials", true);
+   res.header("Access-Control-Allow-Headers", "Content-Type");
+   next();
+});
+
+app.use(
+   cors({
+      origin: (origin, callback) => callback(null, true),
+      credentials: true,
+   })
+);
+
+app.use(express.json({ limit: "1mb" }));
+
+app.use(express.urlencoded({ limit: "1mb", extended: true }));
+
+app.set("secretKey", JWT_SECRET);
+
+
+app.use("*", (req, res, next) => {
+   const error = new Error();
+   error.status = 404;
+   error.message = "Route not found";
+   return next(error);
+});
+
+app.use((error, req, res, next) => {
+   return res
+      .status(error.status || 500)
+      .json(error.message || "Unexpected error");
+});
+
+app.disable("x-powered-by");
+
 app.listen(PORT, () => {
-   console.log(`Server running on port -> http://localhost:${PORT}`)
-})
+   console.log(`Server running on port -> http://localhost:${PORT}`);
+});
